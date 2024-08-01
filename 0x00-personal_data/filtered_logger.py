3f6c85103ea3fb03e06fa2c71c1f0f88effb00a7
AshTechs <ashbelh@gmail.com>
2024-08-01 03:05:44 +0000

    Done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,23 +3,22 @@
 Filtered Logger module
 """
 
-import logging
-import re
 import os
-import mysql.connector
+import re
+import logging
 from typing import List
+import mysql.connector
 from mysql.connector import connection
-
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """Redacts fields in the log message"""
+    """Redacts fields in the log message."""
     for field in fields:
-        message = re.sub(f"{field}=.*?{separator}",
-                         f"{field}={redaction}{separator}", message)
+        pattern = f"{field}=(.*?){separator}"
+        message = re.sub(pattern, f"{field}={redaction}{separator}", message)
     return message
 
 
@@ -31,29 +30,33 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        self.fields = fields
+        """Initialize the formatter."""
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Format the log record."""
         original_message = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             original_message, self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
-    """Returns a logger object"""
+    """Returns a logger object."""
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
+
     stream_handler = logging.StreamHandler()
     formatter = RedactingFormatter(fields=PII_FIELDS)
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
+
     return logger
 
 
 def get_db() -> connection.MySQLConnection:
-    """Returns a connector to the database"""
+    """Returns a connector to the database."""
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
