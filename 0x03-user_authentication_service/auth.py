99283339d7b4607f422a80c5af50758b07facfb7
AshTechs <ashbelh@gmail.com>
2024-08-15 01:05:41 +0000

    Corrected

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,19 +3,52 @@
 Auth module
 """
 
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
-def _hash_password(password: str) -> bytes:
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    Hashes a password using bcrypt and returns the hashed password as bytes.
 
-    Args:
-        password (str): The password to hash.
+    def __init__(self):
+        self._db = DB()
 
-    Returns:
-        bytes: The hashed password.
-    """
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+    def _hash_password(self, password: str) -> bytes:
+        """
+        Hashes a password using bcrypt and returns the hashed password as bytes
+
+        Args:
+            password (str): The password to hash.
+
+        Returns:
+            bytes: The hashed password.
+        """
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers a new user with the provided email and password.
+
+        Args:
+            email (str): The user's email.
+            password (str): The user's password.
+
+        Returns:
+            User: The newly created User object.
+
+        Raises:
+            ValueError: If a user with the provided email already exists.
+        """
+        try:
+            # Check if user already exists
+            self._db._session.query(User).filter_by(email=email).one()
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            # User does not exist, create a new one
+            hashed_password = self._hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
