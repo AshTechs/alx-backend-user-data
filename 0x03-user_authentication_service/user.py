e096e7395998a9962f5bcbebb69d519bba9358a0
AshTechs <ashbelh@gmail.com>
2024-08-15 00:51:58 +0000

    Corrected

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
 """
-User module
+User model for the users table.
 """
-from sqlalchemy import Column, Integer, String, Base
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
+
 class User(Base):
-    """User class
-    Represents a user in the database.
+    """
+    SQLAlchemy model for the users table.
     """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+
+if __name__ == "__main__":
+    engine = create_engine('sqlite:///test.db')
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
